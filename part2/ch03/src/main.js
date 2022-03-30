5c4c884ad064e4f527920690a40d174f9a3d5682
sloppy-cat <js3891129@gmail.com>
2022-03-30 13:26:16 +0900

    setting eslint

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import MainPage from './pages/MainPage.vue'
-import LoginPage from './pages/LoginPage.vue'
-import ProfilePage from './pages/ProfilePage.vue'
-
+import routes from './routes'
 Vue.config.productionTip = false
 
 
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes: [
-    { path: '/', component: MainPage },
-    { path: '/login', component: LoginPage },
-    { path: '/profile', component: ProfilePage }
-  ]
+  routes
 })
 new Vue({
   router,
