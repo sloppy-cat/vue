b7eba170ef148ab973034217e4721488b360bf8c
sloppy-cat <js3891129@gmail.com>
2022-03-31 10:06:54 +0900

    part3 init

--- part2/ch03/src/pages/LoginPage.vue
+++ part2/ch03/src/pages/LoginPage.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      login: <input v-model="login" type="text" />
+      password: <input v-model="password" type="password" />
+      <button type="submit">Login</button>
+    </form>
   </div>
 </template>
 <script>
 export default {
-
+  data() {
+    return {
+      login: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      alert(`Login as ${this.login}`, `Password is ${this.password}`)
+      // this.$http.post('/login', {
+      //   login: this.login,
+      //   password: this.password
+      // }).then(response => {
+      //   console.log(response)
+      // })
+    }
+  }
 }
 </script>
 <style lang="">
--- part2/ch03/src/routes/index.js
+++ part2/ch03/src/routes/index.js
@@ -5,6 +5,16 @@ import ProfilePage from '../pages/ProfilePage.vue'
 const routes = [
   { path: '/', component: MainPage },
   { path: '/login', component: LoginPage },
-  { path: '/profile', component: ProfilePage }
+  {
+    path: '/profile',
+    component: ProfilePage,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('user')) {
+        next(to)
+      } else {
+        next('/login')
+      }
+    }
+  }
 ]
 export default routes
